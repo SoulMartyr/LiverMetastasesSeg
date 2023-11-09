da7e8eda791ae7f99be58ad8ad03eb90f16ec86b
lab_199 <454778393@qq.com>
2023-11-09 14:37:07 +0800

    add test unet3d

--- models/models_3d/__init__.py
+++ models/models_3d/__init__.py
@@ -1,4 +1,5 @@
 from models.models_3d.UNet3D import UNet3D
+from models.models_3d.UNet3d import UNet3d
 from models.models_3d.UXNet3D import UXNet3D
 from models.models_3d.SwinUTR import SwinUTR
 from models.models_3d.MedNeXt import MedNeXt
